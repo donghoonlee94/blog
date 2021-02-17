5cf6c9bdd459f87a4b8030f7e952fd3164c2e735
unknown <94donghoon.lee@gmail.com>
2021-02-17 20:13:48 +0900

    Add routes, post reducer, post saga, router

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -2,6 +2,8 @@ import { all, fork } from 'redux-saga/effects'
 import axios from 'axios'
 
 import authSaga from './authSaga'
+import postSaga from './postSaga';
+
 import dotenv from 'dotenv'
 dotenv.config();
 
@@ -10,6 +12,7 @@ axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL
 // all 은 배열 안의 여러 사가를 동시에 실행시켜줍니다.
 export default function* rootSaga() {
   yield all([
-    fork(authSaga)
+    fork(authSaga),
+    fork(postSaga)
   ]);
 }
--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -2,13 +2,29 @@ import React, { Fragment } from'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer';
 import AppNavBar from '../components/AppNavBar';
+import { Container } from 'reactstrap';
+import PostCardList from './normalRoute/PostCardList';
+import PostWrite from './normalRoute/PostWrite';
+import PostDetail from './normalRoute/PostDetail';
+import Search from './normalRoute/Search';
+import CategoryResult from './normalRoute/CategoryResult';
+import { Redirect, Switch, Route } from 'react-router-dom';
 
 const MyRouter = () => (
   // vue teamplete? 같은 개념, 
   <Fragment>
     <AppNavBar />
     <Header />
-    <h1>Hello body</h1>
+    <Container id="main-body">
+      <Switch>
+        <Route path="/" exact component={PostCardList} />
+        <Route path="/post" exact component={PostWrite} />
+        <Route path="/post/:id" exact component={PostDetail} />
+        <Route path="/post/category/:categoryName" exact component={CategoryResult} />
+        <Route path="/search/:searchTerm" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Container>
     <Footer />
   </Fragment>
 )
